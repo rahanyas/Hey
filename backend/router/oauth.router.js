e9fb14a44f8a70976a4d70c12124d218fb706f45
rahanyas <rahanyas3@gmail.com>
2025-12-02 16:55:58 +0530

    changed redirect url

--- backend/router/oauth.router.js
+++ backend/router/oauth.router.js
@@ -29,7 +29,7 @@ router.get('/google/callback', passport.authenticate('google', {
 
     const redirectUrl =  process.env.PROD_URI 
     console.log('redirect url : ', redirectUrl );
-    return res.redirect(redirectUrl)
+    return res.redirect(`${redirectUrl}/auth/google/success`)
     
 });
 
