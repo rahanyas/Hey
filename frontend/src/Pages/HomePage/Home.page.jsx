42209effc7a114e65f40f88fca105577fa669d6a
rahanyas <rahanyas3@gmail.com>
2025-12-24 16:50:28 +0530

    added styles to home page (mobile ui)

--- frontend/src/Pages/HomePage/Home.page.jsx
+++ frontend/src/Pages/HomePage/Home.page.jsx
@@ -11,33 +11,43 @@ const Home = () => {
 
     return (
         <section id="container">
-            {/* navbar */}
-            <header>
-                {
-                    showSearch === true  ?  (
-                        <input type="text" placeholder='search friends' />
-                    ) : (
-                        <h2>serach friends</h2>
-                    )
-                }
-               <span onClick={() => setShowSearch((prev) => !prev)}><FaSearch /></span> 
-            </header>
-
-            <main className='friendsBox'>
-                {/* add friend btn showing when user has no friends */}
-                <div className='no-friends'>
-                    <h2>add friends <br /> to get chat started</h2>
-                {
-                    friends.length <= 0 && (
-                        <AddFriendBtnCompo className='add-btn'>add friend</AddFriendBtnCompo>
-                    )
-                } 
+            {/* Top Bar */}
+            <header className="top-bar">
+                <div className="search-area">
+                    {
+                        showSearch ? (
+                            <input type="text" placeholder="Search friends..." />
+                        ) : (
+                            <h2>Search friends</h2>
+                        )
+                    }
                 </div>
 
+                <button 
+                    className="icon-btn"
+                    onClick={() => setShowSearch(prev => !prev)}
+                >
+                    <FaSearch />
+                </button>
+            </header>
+
+            {/* Main */}
+            <main className="friendsBox">
+                <div className="no-friends">
+                    <h2>Add friends</h2>
+                    <p>to get chat started</p>
+
+                    {
+                        friends.length <= 0 && (
+                            <AddFriendBtnCompo className="add-btn">
+                                Add friend
+                            </AddFriendBtnCompo>
+                        )
+                    }
+                </div>
             </main>
-            
         </section>
-    )
+    );
 };
 
-export default Home
+export default Home;
